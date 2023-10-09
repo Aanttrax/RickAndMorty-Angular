2dc4177e3a4aa93db32da7cbc53dc35e232bd650
Aanttrax <Arielrt20@gmail.com>
2023-10-09 04:41:52 -0400

    feat: :sparkles: adicion de las pag de about y notFound

--- src/app/pages/characters/characters-list/characters-list-routing.module.ts
+++ src/app/pages/characters/characters-list/characters-list-routing.module.ts
@@ -5,7 +5,9 @@ import { CharactersListComponent } from './characters-list.component';
 const routes: Routes = [{ path: '', component: CharactersListComponent }];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule],
 })
-export class CharactersListRoutingModule { }
+export class CharactersListRoutingModule {
+
+}
--- src/app/pages/characters/characters-list/characters-list.component.ts
+++ src/app/pages/characters/characters-list/characters-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataService } from '@app/shared/services/data.service';
 
 @Component({
   selector: 'app-characters-list',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./characters-list.component.scss']
 })
 export class CharactersListComponent {
-
+    characters$ = this.dataSvc.characters$;
+    constructor(private dataSvc: DataService) {}
 }
